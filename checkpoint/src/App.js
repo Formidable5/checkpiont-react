01efd29e6fa28a916ae5263876f2416767b33f69
Formidable5 <itzformidablemimi@gmail.com>
2024-09-04 12:59:24 +0100

    react-checkpoint

---
+++ checkpoint/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import PlayerList from './PlayerList';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>FIFA PLAYER CARD</h2>
+      <PlayerList />
+    </div>
+  );
+}
+
+export default App;
---
+++ checkpoint/src/players.js
@@ -0,0 +1,45 @@
+import img1 from './assets/messi.jpeg'
+import img2 from './assets/ronaldo.jpeg'
+import img3 from './assets/osemen.jpeg'
+import img4 from './assets/mbape.jpeg'
+import './Player.css'
+
+
+
+
+const players = [
+    {
+        name: "Lionel Messi",
+        team: "inter maimie",
+        nationality: "Argentina",
+        jerseyNumber: '10',
+        age: 38,
+        image: img1,
+    },
+    {
+        name: "Ronaldo",
+        team: "Al nasar",
+        nationality: "Portugal",
+        jerseyNumber: '7',
+        age: 39,
+        image: img2,
+    },
+    {
+        name: "Osimen",
+        team: "Napoli",
+        nationality: "Nigeria",
+        jerseyNumber: '9',
+        age: 25,
+        image: img3,
+    },
+    {
+        name: "Mbappe",
+        team: "Real Madrid",
+        nationality: "France",
+        jerseyNumber: '9',
+        age: 25,
+        image: img4,
+    },
+]
+
+export default players
